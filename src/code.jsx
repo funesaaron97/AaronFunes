da98ae98f09ce08e7b754c7dbf607904d7164eb6
funesaaron97 <funesaaron97@gmail.com>
2023-01-10 20:18:17 -0600

    Added Towers of Hanoi to Code Section

--- src/code.jsx
+++ src/code.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import tictactoe from "./tictactoe.PNG";
 import websitepic from "./websitepic.PNG";
+import towersofhanoi from "./Towers_of_Hanoi_React.png";
 
 class Code extends React.Component {
     render() {
@@ -25,8 +26,18 @@ class Code extends React.Component {
                             </div>
                             <div class="column2">
                                 <h1>Portfolio Website</h1>
-                                <p> I built this current website with javascript, html, css, and the React framework. In this project, I utilized React's core feature of jsx components and routed them together using the React Router library. </p>
+                                <p> I built this current website with javascript, html, css, and the React framework. In this project, I utilized React's core feature of jsx class components and routed them together using the React Router library. </p>
                                 <p> Click the image to check out the source code!</p>
+                            </div>
+                        </div>
+                        <div class="codestuff columns">
+                            <div class ="wslink column1">
+                                <a href="https://funesaaron97.github.io/towers_of_hanoi/"><img src={towersofhanoi} width="400" height="400"/></a>
+                            </div>
+                            <div class="column2">
+                                <h1>Towers of Hanoi</h1>
+                                <p> I recreated the mathematical game Towers of Hanoi in both ReactJS and Python. For the Python method, I utilized Linked Lists and Nodes as a way to manipulate the abstract Stack Data Structure. In the ReactJS version, I modified the stack manipulation using arrays and then utilized the useState hook to affect the DOM with Javascript.  </p>
+                                <p> Click the image to check out the game! Click <a href ="https://github.com/funesaaron97/towers_of_hanoi">here</a> for source code! Click <a href = "https://github.com/funesaaron97/Towers-of-Hanoi">here</a> for the Linked Lists Python version source code!</p>
                             </div>
                         </div>
                         <div class="codestuff columns">
