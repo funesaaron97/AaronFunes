6d51871739e9a64be50cc1fa4c1103f9f012486f
funesaaron97 <funesaaron97@gmail.com>
2023-01-14 17:54:17 -0600

    Added Crytpo Currency Price Monitor to the Code Section

--- src/code.jsx
+++ src/code.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import tictactoe from "./tictactoe.PNG";
 import websitepic from "./websitepic.PNG";
 import towersofhanoi from "./Towers_of_Hanoi_React.png";
+import crypto from "./crypto.PNG";
 
 class Code extends React.Component {
     render() {
@@ -38,6 +39,16 @@ class Code extends React.Component {
                                 <h1>Towers of Hanoi</h1>
                                 <p> I recreated the mathematical game Towers of Hanoi in both ReactJS and Python. For the Python method, I utilized Linked Lists and Nodes as a way to manipulate the abstract Stack Data Structure. In the ReactJS version, I modified the stack manipulation using arrays and then utilized the useState hook to affect the DOM with Javascript.  </p>
                                 <p> Click the image to check out the game! Click <a href ="https://github.com/funesaaron97/towers_of_hanoi">here</a> for source code! Click <a href = "https://github.com/funesaaron97/Towers-of-Hanoi">here</a> for the Linked Lists Python version source code!</p>
+                            </div>
+                        </div>
+                        <div class="codestuff columns">
+                            <div class ="wslink column1">
+                                <a href="https://funesaaron97.github.io/price_monitor/"><img src={crypto} width="400" height="400"/></a>
+                            </div>
+                            <div class="column2">
+                                <h1>Crypto Currency Price Monitor</h1>
+                                <p> I programmed a Crypto Currency Price checker utilizing Coingecko's free API services. I constructed the App using ReactJS's core features of useEffect and functional components, in addition to utilizing the package Axios to deal with promises and JSON objects.  </p>
+                                <p> Click the image to check out the App! Click <a href ="https://github.com/funesaaron97/price_monitor">here</a> for source code!</p>
                             </div>
                         </div>
                         <div class="codestuff columns">
