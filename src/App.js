52f9e06aa8118e06fc452627a116d2da760e3c5e
funesaaron97 <funesaaron97@gmail.com>
2023-01-25 14:03:30 -0600

    Replaced ReactRouter with HashRouter to fix refresh error in github pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './about';
 import Code from './code';
 import Music from './music';
 import Media from './medialinks';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
